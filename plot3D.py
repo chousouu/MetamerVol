3db7985635a875ec49df8249e0571daff7824da1
Yasin Mamedov <mamedov.iakh@phystech.edu>
2023-07-16 16:08:40 +0300

    refactored, added 2 functions for plotting with/without prediction color

--- plot3D.py
+++ plot3D.py
@@ -1,28 +1,29 @@
 import plotly.offline as py
 import plotly.graph_objs as go
 import numpy as np
+import MetamerMismatch as mm
 
-def mesh_from_hull(hull, color = None, name = None): #color = None works, it sets random colors
-	vertices = hull.points 
-	faces = hull.simplices
-	
-	if name == 'ocs':
-		color = 'black'
+def plot_metamer_body(mmbs, ocs):
+	def mesh_from_hull(hull, name = None): #color = None works, it sets random colors
 
-	mesh = \
-		go.Mesh3d(
-		   x=np.array(vertices)[:,0],
-		   y=np.array(vertices)[:,1],
-		   z=np.array(vertices)[:,2],
-		   i=np.array(faces)[:,0],
-		   j=np.array(faces)[:,1],
-		   k=np.array(faces)[:,2],
-		   opacity=0.5,
-		   color = color, name = name) #,  alphahull = 0 # ?
-	
-	return mesh
+		vertices = hull.points 
+		faces = hull.simplices
+		
+		color = None if name != 'ocs' else 'black'
 
-def plot_convex_hulls(hulls_dict):
+		mesh = \
+			go.Mesh3d(
+			x=np.array(vertices)[:,0],
+			y=np.array(vertices)[:,1],
+			z=np.array(vertices)[:,2],
+			i=np.array(faces)[:,0],
+			j=np.array(faces)[:,1],
+			k=np.array(faces)[:,2],
+			opacity=0.5,
+			color = color, name = name) #,  alphahull = 0 # ?
+		
+		return mesh
+
 	layout = \
 		go.Layout(
 	        scene = dict(
@@ -33,13 +34,28 @@ def plot_convex_hulls(hulls_dict):
 
 	meshes = []
 
-	for hull_name, hull_class in hulls_dict.items():
-		meshes += [mesh_from_hull(hull_class, name = hull_name)]
+	meshes.append(mesh_from_hull(ocs, 'ocs'))
+
+	for mmb in mmbs:
+		meshes.append(mesh_from_hull(mmb))
 
 	fig = go.Figure(data = meshes, layout = layout)
 	fig.update_traces(showlegend=True, showscale=False)
 
+
+	return fig
+
+def plot_scene(ocs, colors_mmb, color_prediction = None):
+	fig = plot_metamer_body(mmbs = colors_mmb, ocs = ocs)
+
+	if color_prediction is not None:
+		fig.add_trace(go.Scatter3d(x = [color_prediction[0]], y = [color_prediction[1]], z = [color_prediction[2]],
+			     mode = 'markers', name = 'predicted color'))
+		
 	fig.show()
 
 	return
-	# py.plot(fig, filename=filename)
+
+#maybe i want to use it?..
+def rgb_to_string(rgb):
+    return '_'.join([(str(format(_, '.3f'))) for _ in rgb])
