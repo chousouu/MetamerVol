62f897811ab0f6615d9337616be6b43ddc41ff4b
Yasin Mamedov <mamedov.iakh@phystech.edu>
2023-07-16 16:09:59 +0300

    TODO: think of creating another file for some functions

--- MetamerMismatch.py
+++ MetamerMismatch.py
@@ -1,8 +1,12 @@
 import numpy as np
 import scipy 
+import pygel3d.gl_display as gd
+from pygel3d import hmesh as hm 
+from scipy.spatial import ConvexHull
 
 NUMBER_OF_SAMPLES = 1000
 WAVELENGTH_RESOLUTION = 10
+COLOR_EPSILON = 0.001
 
 # GENERAL TODO'S (important -> less important):
 # 1.PEP8 style guide check
@@ -28,7 +32,6 @@ def sample_unit_sphere(color_sys, wavelengths, sample_amount):
         wave = (wave + 1) % wavelengths_size
 
     return np.reshape(np.array(k_sampled), (vector_size, sample_amount)).T
-
 
 def solve_linear_problem(objective_func_coef, constrain_func = None, 
                          constrain_func_value = None, bounds = None):
@@ -61,7 +64,6 @@ def solve_linear_problem(objective_func_coef, constrain_func = None,
 
     return (x_max, x_min)
 
-
 def get_mmb_points(metameric_color,  #<- metameric colors phi(r) in phi-space
                illum_phi, sens_phi,
                illum_psi, sens_psi,
@@ -69,9 +71,9 @@ def get_mmb_points(metameric_color,  #<- metameric colors phi(r) in phi-space
 
 #TODO: HARDCODED NEED TO BE FIXED! (maybe refactor functions)
     wavelengths = np.arange(400, 701, WAVELENGTH_RESOLUTION) 
+    S_phi = get_colour_sys(illum_phi, sens_phi)
+    S_psi = get_colour_sys(illum_psi, sens_psi) # q x N, q - wavelength resolution
     
-    S_phi = np.stack([illum_phi * sens for sens in sens_phi.T], axis = 1) #color sys; size = q x N
-    S_psi = np.stack([illum_psi * sens for sens in sens_psi.T], axis = 1) #
     S = np.concatenate((S_phi, S_psi), axis = 1) #q x 2N; q - wavelength resolution
 
     mmb_extr_points = []
@@ -91,9 +93,8 @@ def get_mmb_points(metameric_color,  #<- metameric colors phi(r) in phi-space
 
     return mmb_extr_points
 
-
 def get_ocs_points(illum, sens, sampling_resolution = NUMBER_OF_SAMPLES):
-    S = np.stack([illum * sens_ for sens_ in sens.T], axis = 1)
+    S = get_colour_sys(illum, sens)
 
 #TODO: HARDCODED NEED TO BE FIXED! (maybe refactor functions)
     wavelengths = np.arange(400, 701, WAVELENGTH_RESOLUTION) 
@@ -106,7 +107,6 @@ def get_ocs_points(illum, sens, sampling_resolution = NUMBER_OF_SAMPLES):
 
         # scale so the brightest illum color response == 1  
         scale = np.max(np.dot(sens.T, illum))
-        # scale = np.max(S)
         
         max_color = np.dot(max_reflectance, S) / scale
         min_color = np.dot(min_reflectance, S) / scale
@@ -130,9 +130,57 @@ def get_ocs_points(illum, sens, sampling_resolution = NUMBER_OF_SAMPLES):
 #                    illum_phi = D65_Illuminant, illum_psi = D65_Illuminant,
 #                    sens_phi = canon_sens, sens_psi = cie1964))} )
 
+#---------------------------------------------------------------------------------------------------------------
+#TODO: find a way to remove degenerated triangles without transfering convexhull->manifold like def dist() 
+def _distance(points, point):
+    m = hm.Manifold()
+    for i in range(0, len(points), 3):
+        m.add_face(points[i : i + 3])
+
+    dist = hm.MeshDistance(m)
+    distr = dist.signed_distance(point)
+    return distr
+#---------------------------------------------------------------------------------------------------------------
+
+def hull_from_points(surface_points):
+	#maybe add raisevalue if points are added as manifold class
+	return ConvexHull(surface_points)
+
+def _view_Manifold(m):
+    gd.Viewer().display(m)
+    return
+
+def dist(hull, point): #temp(?) version of func
+    m = hm.Manifold()
+
+    for s in hull.simplices:
+        m.add_face(hull.points[s])
+
+    dist = hm.MeshDistance(m).signed_distance(point)
+
+    return dist
 
 def get_unique_colors(image):
-    pass
+    """
+    receives 2D (width * length, 3) or 3D (length, width, 3) image array 
+    and returns unique colors of the image.
+    """
+    def is_equal(color_one, color_two):
+        return all(abs(color_two - color_one) < COLOR_EPSILON)
+    def is_unique(color, new_colors):
+        for color in new_colors:
+            if is_equal(rgb_pixel, color):
+                return False
+        return True
+    
+    image = np.reshape(image, (-1, 3))
+
+    new_colors = [image[0, :]]
+    for rgb_pixel in image:
+        if is_unique(rgb_pixel, new_colors):
+            new_colors.append(rgb_pixel)
+
+    return np.array(new_colors)
 
 def get_colour_sys(illum, sens):
     return np.stack([illum * sens for sens in sens.T], axis = 1)    
