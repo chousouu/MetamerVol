776c020351bc705a9d254c5b83b15b5cdbe99867
Yasin Mamedov <mamedov.iakh@phystech.edu>
2023-07-18 19:08:32 +0300

    minor changes

--- HSIconvert.py
+++ HSIconvert.py
@@ -4,10 +4,9 @@ import h5py
 from pathlib import Path
 import argparse
 from os.path import splitext
-import pandas as pd
 from PIL import Image 
 
-PATH_LI = "/home/yasin/iitp/li_ds/"
+PATH_LI = "/home/yasin/iitp/li_ds"
 ILLUM_PATH = "cie/std.csv"
 SAVE_IMAGES = '/home/yasin/iitp/tempdir'
 
@@ -17,7 +16,6 @@ def read_h5(h5_path : Path, wv_hsi: np.ndarray):
         hsi = awb.SpectralFunction(hsi_y, wv_hsi, dtype=np.float32)
         
         return hsi
-
 
 def add_ill(data, ill_path): ## returns SpectralFunction
     illums_path = awb.spectral_data.illuminances_path / ILLUM_PATH
@@ -33,10 +31,10 @@ def view(img):
         img = (img * 255).astype(np.uint8)
     return Image.fromarray(img)
 
-
-def convert2png(image_name, 
-                sensitivities : str = 'canon', illuminance : str = 'Standard Illuminant D65', 
-                outdir : Path = Path(SAVE_IMAGES)):
+def get_colour_responses(image_name, 
+                sensitivity_name : str = 'canon', 
+                illuminance : str = 'Standard Illuminant D65', 
+                outdir : Path = None):
     awb.SpectralFunction.lambdas = np.linspace(400, 730, num=34, endpoint=True) 
     path_to_hsi = PATH_LI + '/' + image_name
 
@@ -45,49 +43,37 @@ def convert2png(image_name,
 
     with_ill = add_ill(data, illuminance)
 
-    if sensitivities == 'canon':
-        cam = awb.SpectralSensitivies.from_csv(
+    sensitivities = None 
+    if sensitivity_name == 'canon':
+        sensitivities = awb.SpectralSensitivies.from_csv(
         awb.sensitivies_path / 'iitp/canon600d.csv', 
-        normalized=False,
-        channels_names=awb.STD_CHANNELS_NAMES)
-        img = awb.spectral_rendering.camera_render(with_ill, cam)
-    elif sensitivities == 'xyz':
-        xyz = awb.SpectralSensitivies.from_csv(
+        normalized=False, channels_names=awb.STD_CHANNELS_NAMES)
+    elif sensitivity_name == 'xyz':
+        sensitivities = awb.SpectralSensitivies.from_csv(
         awb.sensitivies_path / 'iitp/xyz_matching_fun.csv',
         normalized=False)
-        img = awb.spectral_rendering.camera_render(with_ill, xyz)
+    else:
+        raise ValueError("There is no such sensitivities!")
 
+    img = awb.spectral_rendering.camera_render(with_ill, sensitivities)
     img = np.clip(img / np.max(img), 0, 1)
 
-    save_path = splitext(outdir)[0] + '/' + image_name + '.png'
-    awb.imsave(Path(save_path), img, unchanged=False, out_dtype=np.uint16, gamma_correction=True)
+    if outdir is not None:
+        save_path = splitext(outdir)[0] + '/' + splitext(image_name)[0] + '.png'
+        awb.imsave(Path(save_path), img, unchanged=False, out_dtype=np.uint16, gamma_correction=True)
     
-    return save_path
+    return img
 
 
 if __name__ == '__main__':    
-    # HSI_name = "2019-09-11_047.h5" # motorcycle
-    # HSI_name = "2019-09-08_007.h5" # another text
-    # HSI_name = "2019-09-09_013.h5" # money
-    # HSI_name = "2019-08-29_021.h5" # cottons
-    # HSI_name = "2019-08-26_023.h5" # (red) watermelon
-    # HSI_name = "2019-08-25_004.h5" # child room
-    # HSI_name = "2019-09-08_015.h5" # text
-    # HSI_name = "2019-08-28_016.h5" # green(red) leaves
-    # HSI_name = "2019-09-18_003.h5" # colorchecker image
-
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', '--outdir', type=Path, default=Path(SAVE_IMAGES))
-    parser.add_argument('-s', '--sensetivities', type=str, default='xyz')
-
+    parser.add_argument('-s', '--sensitivities', type=str, default='canon')
     args = parser.parse_args()
 
     args.outdir.mkdir(exist_ok=True, parents=True)
-
-    # convert2png("2019-09-10_017.h5", sensitivities = 'canon', outdir = args.outdir)
-    # for name in ["2019-09-11_047.h5", "2019-09-08_007.h5", "2019-09-09_013.h5",
-    #              "2019-08-29_021.h5",
-    #             "2019-08-26_023.h5",
-    #             "2019-08-25_004.h5",
-    #             "2019-09-08_015.h5", "2019-08-28_016.h5"]:
-    #     convert2png(name, sensitivities = 'xyz', outdir = args.outdir)
+    
+    for name in ["2019-09-11_047.h5", "2019-09-08_007.h5", "2019-09-09_013.h5",
+                 "2019-08-29_021.h5", "2019-08-26_023.h5", "2019-08-25_004.h5",
+                 "2019-09-08_015.h5", "2019-08-28_016.h5", "2019-09-08_007.h5"]:
+        get_colour_responses(name, sensitivity_name='xyz', outdir = args.outdir)
