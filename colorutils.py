16ebd2c9b0e17398f00c414a5a2a6894af200757
Yasin Mamedov <mamedov.iakh@phystech.edu>
2023-07-19 20:48:53 +0300

    rename idk, reworked functions

---
+++ colorutils.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+COLOR_EPSILON = 1e-4
+
+def clear_array_from_zeros(arr : np.ndarray):
+    if np.all(arr == 0):
+        raise ValueError("Fully zeroed array!")
+
+    return arr[arr.any(axis = 1)]
+
+def find_non_zero_elem(arr : np.ndarray):
+    """
+    works on 1d only
+    """
+    # print(f"\n\n{arr=} ({arr.shape=}), chose {np.nonzero(arr)[0][0]}\n\n")
+    return np.nonzero(arr)[0][0]
+
+
+def get_unique_colors(image):
+    """
+    receives 2D (width * length, 3) or 3D (length, width, 3) image array 
+    and returns unique colors of the image.
+    """
+    def is_equal(color_one, color_two):
+        return np.all(abs(color_two - color_one) < COLOR_EPSILON)
+    def is_unique(color, new_colors):
+        for color in new_colors:
+            if is_equal(rgb_pixel, color):
+                return False
+        return True
+    
+    image = np.reshape(image, (-1, 3))
+
+    new_colors = []
+    for rgb_pixel in image:
+        if is_unique(rgb_pixel, new_colors):
+            new_colors.append(rgb_pixel)
+
+    return np.array(new_colors)
+
+def get_colour_sys(illum, sens):
+    return np.stack([illum * sens for sens in sens.T], axis = 1)    
+
+def rgb_to_string(rgb):
+    return '_'.join([(str(format(_, '.4f'))) for _ in rgb])
