a1a9688f6d0a737023cd861a4f4c8c3056d00119
Yasin Mamedov <mamedov.iakh@phystech.edu>
2023-07-28 03:42:44 +0300

    refactored

--- plot_metamer.py
+++ plot_metamer.py
@@ -2,61 +2,71 @@ import plotly.offline as py
 import plotly.graph_objs as go
 import numpy as np
 import MetamerMismatch as mm
+from colorutils import rgb_to_string
 
-def mesh_from_hull(hull, name = None): #hull : ConvexHull
+def mesh_from_hull(hull, name = None, title_text = None):
     vertices, faces = hull.points, hull.simplices
-    
-    legend_text = 'Metamer body colors'
-    legendgroup = 'group1'
+
     color = None
 	    
     if name == 'ocs':
-        color, legend_text, legendgroup = 'black', 'Object Colour Solid', 'group0.5'
+        color = 'black'
         
     mesh = \
         go.Mesh3d(
         x=np.array(vertices)[:,0], y=np.array(vertices)[:,1], z=np.array(vertices)[:,2],
         i=np.array(faces)[:,0]   , j=np.array(faces)[:,1]   , k=np.array(faces)[:,2],
         opacity=0.5, color = color, name = name,
-        legendgrouptitle_text = legend_text,
-        legendgroup=legendgroup) #,  alphahull = 0 # ?
+        legendgrouptitle_text = title_text,
+        legendgroup = title_text) #,  alphahull = 0 # ?
     
     return mesh
 
+def plot_scene(colors_mmb, color_predictions = None, ocs = None):
+	"""
+    plots scene 
 
-def plot_metamer_body(mmbs, ocs = None): #ocs : ConvexHull = None
-	#mmb : ConvexHull
-    
+    Parameters
+    ----------
+    colors_mmb : array_like
+        array where each element is type ConvexHull
+    color_predictions : (M, 3) array_like, optional
+		scatter plots colors if not 'None'
+	ocs : ConvexHull, optional
+		plots object colour solid if not 'None' 
+
+    Returns
+    -------
+    fig : class Figure
+        returns go.Figure that can be used to add/update currently existing plot.
+    """
 	layout = go.Layout(scene = dict(
 	            xaxis = dict(range = [0,1], title = 'r', showgrid = False),
 	            yaxis = dict(range = [0,1], title = 'g', showgrid = False),
 	            zaxis = dict(range = [0,1], title = 'b', showgrid = False),
 	            aspectmode='cube'), legend_font_size = 20)
-
-	meshes = []
+	
+	fig = go.Figure(layout = layout)
 
 	if ocs is not None:
-		meshes.append(mesh_from_hull(ocs, 'ocs'))
-
-	for mmb in mmbs:
-		meshes.append(mesh_from_hull(mmb))
-
-	fig = go.Figure(data = meshes, layout = layout)
-	fig.update_traces(showlegend=True, showscale=False)
-
-	return fig
-
-def plot_scene(colors_mmb, ocs = None, color_predictions = None):
-	fig = plot_metamer_body(mmbs = colors_mmb, ocs = ocs)
+		fig.add_trace(mesh_from_hull(ocs, name = 'ocs', title_text = 'Object Colour Solid'))
 
 	if color_predictions is not None:
 		for ind, color in enumerate(color_predictions):
+			title_text = rgb_to_string(color)
 			fig.add_trace(go.Scatter3d(x = [color[0]], 
 									   y = [color[1]], 
 									   z = [color[2]],
-						mode = 'markers', name = f'pred color{ind + 1}',
-			            legendgrouptitle_text = 'Predicted Colors',
-			            legendgroup="group2"))
+						mode = 'markers', name = 'Predicted color',
+			            legendgrouptitle_text = title_text,
+			            legendgroup = title_text))
+			fig.add_trace(mesh_from_hull(colors_mmb[ind], name = f'Metamer', title_text = title_text))
+	else: 
+		for ind, color in enumerate(colors_mmb):
+			fig.add_trace(mesh_from_hull(color, name = f"MMB{ind + 1}"))
+	
+		
+	fig.update_traces(showlegend = True)
 
 	fig.show()
 	return fig
